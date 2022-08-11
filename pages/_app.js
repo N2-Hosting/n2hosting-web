4daf5dfb6538872a3012d2d8efbc46c21fd01df4
John Gerome Baldonado <johngerome@gmail.com>
2022-08-11 19:17:45 +0800

    chore: code clean-up (#10)

--- pages/_app.js
+++ pages/_app.js
@@ -3,30 +3,6 @@ import '@master/css';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  // componentDidMount() {
-  //   // grab token value from cookie
-  //   const token = Cookie.get("token");
-
-  //   if (token) {
-  //     // authenticate the token on the server and place set user object
-  //     fetch(`${process.env.NEXT_PUBLIC_API_URL}users/me`, {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     }).then(async (res) => {
-  //       // if res comes back not valid, token is not valid
-  //       // delete the token and log the user out on client
-  //       if (!res.ok) {
-  //         Cookie.remove("token");
-  //         this.setState({ user: null });
-  //         return null;
-  //       }
-  //       const user = await res.json();
-  //       this.setUser(user);
-  //     });
-  //   }
-  // }
-
   return (
     <>
       <Head>
